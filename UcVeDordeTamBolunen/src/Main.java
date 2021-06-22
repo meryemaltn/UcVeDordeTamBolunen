9e5f7ef3d9e6fa67f1903fd79df3d5352bde352c
Meryem Altunkaya <meryemaltunkaya@hotmail.com>
2021-06-22 23:32:04 +0300

    First commit

--- UcVeDordeTamBolunen/src/Main.java
+++ UcVeDordeTamBolunen/src/Main.java
@@ -3,8 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Deðiþken tanýmlýyorum.
-		int n;
-		double toplam;
+		int n, toplam=0 ;
 		
 		//Kullanýcýdan bir sayý istiyorum.
 		Scanner input=new Scanner(System.in);
@@ -15,14 +14,14 @@ public class Main {
 		
 		//For Döngüsü oluþturuyorum.
 		for (int i=0; i<=n ; i++) {
-			if ((i%3==0) && (i%4==0)) {
+			
+			// 3 veya 4'e bölünmeyenler hariç geri kalanlarýn toplamý:
+			if ((i%3!=0) || (i%4!=0)) {
 				continue;
 			}
-			toplam=0.0;
-			toplam= toplam+i;
-			System.out.println("3 ve 4'e Tam Bölünenlerin Toplamý: "+toplam);
-			
+			toplam=toplam+i;
 		}
+		System.out.println("3 ve 4'e Tam Bölünenlerin Toplamý: "+ toplam);
 	}
 }
 
